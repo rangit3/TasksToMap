c527d6033d8d21ee8c0ee15a1a59d060c5e524f2
rangit3 <24866224+rangit3@users.noreply.github.com>
2023-10-08 18:28:03 +0300

    initial code

---
+++ parse_csv.py
@@ -0,0 +1,63 @@
+import time
+import geopy
+import pandas as pd
+
+def get_location_from_address(address):
+    gpsLocation = None
+    locationToLook = address.replace(',', '')
+
+    time.sleep(1)
+    # badStartKeys = ReadFileLines('badstartkeys.txt')
+    # for k in badStartKeys:
+    #     if FromHebrew(k)[1:] in locationToLook.split(' ')[0]:
+    #         locationToLook = ' '.join(locationToLook.split(' ')[1:])
+    get = False
+    try:
+        # get new in here:
+        # https://console.developers.google.com/google/maps-apis/apis/maps-backend.googleapis.com/credentials?project=my-project-1524911120610&duration=PT1H
+        backUpGeolocator = geopy.geocoders.GoogleV3('AIzaSyDujiJfrz1GorKb6p-HrzCE-oL_A8GN058')
+        gpsLocation = backUpGeolocator.geocode(locationToLook)
+        get = True
+
+    except Exception as e:
+        X = 2
+    if not get:
+        try:
+            backUpGeolocator = geopy.geocoders.Bing(
+                'Aj4HplPNElwLP_temgYW0JCF_3Jh0oMYCIUH4yxLK32PwAwP9G2bmTABcMpLwciY')
+            gpsLocation = backUpGeolocator.geocode(locationToLook)
+        except Exception as e:
+            X = 2
+        # backUpGeolocator = geopy.geocoders.GoogleV3('AIzaSyBaEq6z0fZ8RWiPjdQK1X4gvXgYtMYvg9Q')
+        # gpsLocation = backUpGeolocator.geocode(locationToLook)
+        # geolocator = geopy.geocoders.Nominatim()
+        # gpsLocation = geolocator.geocode(locationToLook)
+
+    return gpsLocation
+def parse_csv(path_csv):
+    #must have Address column
+    #should have: Category, ID, Other_Information, Status
+
+    df = pd.read_csv(path_csv, index_col=0)
+    addresses = df['Address'].to_numpy()
+    gps_locatoins = [get_location_from_address(address) for address in addresses]
+    addresses_found = [x.address for x in gps_locatoins]
+    lats = [x.latitude for x in gps_locatoins]
+    longs = [x.longitude for x in gps_locatoins]
+    df['addresses_found']=addresses_found
+    df['lat']=lats
+    df['long']=longs
+
+    df.to_csv('reports_updated.csv')
+
+    x=2
+
+
+
+def main():
+    path_csv = 'reports.csv'
+    parse_csv(path_csv)
+
+
+if __name__ == '__main__':
+    main()
