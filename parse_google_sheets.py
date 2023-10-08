76176208bcaff365d58730a23e4a6f5075ca463b
Donut100 <70750011+DonutByte@users.noreply.github.com>
2023-10-08 21:59:58 +0300

    added option to use google sheets as a source
    the function updates with the address and coordinates in-place
    
    TODO:
    - have clear instructions how to get the credentials.json

---
+++ parse_google_sheets.py
@@ -0,0 +1,98 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import parse_csv
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1XdHPZfxvCXPfMgnPLUfjwQaibv6KuguEUbNslr8W7vU'
+SAMPLE_RANGE_NAME = 'A1:1'
+
+
+def get_column_length(sheet) -> int:
+    """
+    Finds the last column in use
+    Args:
+        sheet: the sheet where the data is
+
+    Returns:
+        the index, -1 if no data is found
+    """
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        return -1
+
+    return len(values[0])
+
+
+def parse_sheet(sheet):
+    column_length = get_column_length(sheet)
+    last_column = chr(ord('A') + column_length)
+    print(last_column)
+    range = f'A1:' + last_column
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=range).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+        return
+
+    headers = values[0]
+    address_column_index = headers.index('Address')
+    new_data = [['addresses_found', 'lat', 'long'   ]]
+    update_range = chr(ord('A') + column_length + 1) + '1'
+    for idx, row in enumerate(values[1:]):
+        address = parse_csv.get_location_from_address(row[address_column_index])
+        new_data.append([address.address, address.latitude, address.longitude])
+
+    print(sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=update_range,
+                                valueInputOption='USER_ENTERED',
+                                body={'values': new_data}).execute())
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        sheet = service.spreadsheets()
+        parse_sheet(sheet)
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
