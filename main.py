4aaf8d0ead5bf555974f35d70dfece3e0ff49c78
Ran <stam>
2023-10-09 15:27:52 +0300

    refactor code

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from parse_csv import parse_csv
 
 def main():
     path_csv = 'reports.csv'
-    parse_csv(path_csv)
+    address_col = 'Address'
+    parse_csv(path_csv, address_col = address_col)
 
 if __name__ == '__main__':
     main()
