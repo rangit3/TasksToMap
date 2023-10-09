a22bb6d7fdb61b82186c3c27cb9782c0761d0363
Ran <stam>
2023-10-09 14:00:20 +0300

    fixes for empty rows

--- parse_csv.py
+++ parse_csv.py
@@ -36,13 +36,19 @@ def parse_csv(path_csv):
     if not ('lat' in list(df)) and not('long' in list(df)):
         df["lat"] = math.nan
         df["long"] = math.nan
-    for i in range(len(df)):
-        if math.isnan(df.loc[i, "lat"]) or math.isnan(df.loc[i, "long"]):
-            address = df.loc[i, "Address"]
+    empty_rows = df['Address'].isnull()
+    for i, row in df.iterrows():
+        if empty_rows[i]:
+            continue
+        if math.isnan(row["lat"]) or math.isnan(row["long"]):
+            address = row["Address"]
             gps_location = get_location_from_address(address)
-            df.loc[i, "lat"] = gps_location.latitude
-            df.loc[i, "long"] = gps_location.longitude
-            df.loc[i, "address_found"] = gps_location.address
+            if gps_location is None:
+                print(f'address was not found for {address}')
+            else:
+                df.at[i,"lat"] = gps_location.latitude
+                df.at[i,"long"] = gps_location.longitude
+                df.at[i,"address_found"] = gps_location.address
 
     df.to_csv('reports_updated.csv')
 
